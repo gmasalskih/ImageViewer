201a27732f8e73cba69f3422f052b8a0529d9dcd
gmasalskih <gmasalskih@mail.ru>
2018-04-24 23:24:06 +0300

    v.0.0.3

--- app/src/main/java/com/example/gmasalskikh/imageviewer/View/MainActivity.kt
+++ app/src/main/java/com/example/gmasalskikh/imageviewer/View/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.gmasalskikh.imageviewer
+package com.example.gmasalskikh.imageviewer.View
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gmasalskikh.imageviewer.R
 
 class MainActivity : AppCompatActivity() {
 
