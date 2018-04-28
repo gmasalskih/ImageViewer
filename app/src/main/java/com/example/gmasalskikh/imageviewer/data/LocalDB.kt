6ad981dddd6da35f58f20c40ad266c3b82596ed8
gmasalskikh <gmasalskikh@mail.ru>
2018-04-28 13:31:34 +0300

    v 0.0.6

--- app/src/main/java/com/example/gmasalskikh/imageviewer/data/LocalDB.kt
+++ app/src/main/java/com/example/gmasalskikh/imageviewer/data/LocalDB.kt
@@ -1,3 +1,3 @@
 package com.example.gmasalskikh.imageviewer.data
 
-val db = listOf(1,2,3,4,5,6,7,8,9)
+val db = (1..1000).toList()
--- app/src/main/java/com/example/gmasalskikh/imageviewer/web/RestApi.kt
+++ app/src/main/java/com/example/gmasalskikh/imageviewer/web/RestApi.kt
@@ -8,14 +8,20 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 
 object RestApi {
+
+    private const val TIMEOUT: Long = 30
+
     private val okHttpClient = OkHttpClient.Builder()
             .addInterceptor {
                 Log.d("URL", it.request().url().toString())
                 it.proceed(it.request().newBuilder().build())
-            }.build()
+            }.readTimeout(TIMEOUT, TimeUnit.SECONDS)
+            .build()
+
     private val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .client(okHttpClient)
