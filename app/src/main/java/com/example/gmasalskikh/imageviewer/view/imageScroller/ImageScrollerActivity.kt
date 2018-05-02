8303d23a5a7cc56f0377c3edeb0529d42d9d99de
gmasalskih <gmasalskih@mail.ru>
2018-05-02 19:58:45 +0300

    v.0.0.9

--- app/src/main/java/com/example/gmasalskikh/imageviewer/view/imageScroller/ImageScrollerActivity.kt
+++ app/src/main/java/com/example/gmasalskikh/imageviewer/view/imageScroller/ImageScrollerActivity.kt
@@ -1,35 +1,21 @@
 package com.example.gmasalskikh.imageviewer.view.imageScroller
 
 import android.annotation.SuppressLint
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.support.v7.app.AppCompatActivity
 import com.example.gmasalskikh.imageviewer.R
+import com.example.gmasalskikh.imageviewer.view.BaseActivity
 import org.koin.android.ext.android.inject
 import org.koin.android.ext.android.releaseContext
 
-class ImageScrollerActivity : AppCompatActivity() {
+class ImageScrollerActivity : BaseActivity() {
 
-    private val contextName = this.javaClass.simpleName
-    private val presenter: BaseImageScrollerFragment by inject()
+    private val imageScrollerFragment: ImageScrollerFragment by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image_scroller)
-        if (supportFragmentManager.findFragmentById(R.id.image_scroller_container) == null){
-            supportFragmentManager
-                    .beginTransaction()
-                    .add(R.id.image_scroller_container, presenter)
-                    .commit()
-
-        }
-
+        setFragment(R.id.image_scroller_container, imageScrollerFragment)
     }
 
-    @SuppressLint("MissingSuperCall")
-    override fun onPause(){
-        Log.d("aaa", contextName)
-        releaseContext(contextName)
-        super.onPause()
-    }
 }
--- app/src/main/java/com/example/gmasalskikh/imageviewer/view/imageScroller/ImageScrollerFragment.kt
+++ app/src/main/java/com/example/gmasalskikh/imageviewer/view/imageScroller/ImageScrollerFragment.kt
@@ -1,26 +1,58 @@
 package com.example.gmasalskikh.imageviewer.view.imageScroller
 
+import android.content.Intent
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import com.example.gmasalskikh.imageviewer.R
+import com.example.gmasalskikh.imageviewer.data.Item
 import com.example.gmasalskikh.imageviewer.data.ItemProvider
+import com.example.gmasalskikh.imageviewer.view.imageViewer.ImageViewerActivity
 import kotlinx.android.synthetic.main.item_image_scroller.view.*
+import kotlinx.android.synthetic.main.recycler_view_image_scroller.view.*
 import org.koin.android.ext.android.inject
 
-class ImageScrollerFragment : BaseImageScrollerFragment() {
+class ImageScrollerFragment : Fragment() {
 
-    private val spanCount = 2
-
-    override val itemProvider: ItemProvider by inject()
-    override val layoutItem: Int = R.layout.item_image_scroller
-    override val layoutManager: RecyclerView.LayoutManager = GridLayoutManager(activity, spanCount)
-    override fun bind(position: Int, itemView: View) {
-        itemView.item_text.text = itemProvider.getItem(position).toString()
-    }
+    private val itemProvider: ItemProvider by inject()
+    private val presenter:BaseImageScrollerPresenter by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val view = inflater.inflate(R.layout.recycler_view_image_scroller, container, false)
+        view.recycler_view_image_scroller_container.layoutManager = GridLayoutManager(activity, 2)
+        view.recycler_view_image_scroller_container.adapter = Adapter()
+        return view
+    }
+
+    private inner class Adapter: RecyclerView.Adapter<ImageScrollerFragment.Holder>(){
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+            return Holder(LayoutInflater.from(parent.context).inflate(R.layout.item_image_scroller, parent, false))
+        }
+
+        override fun getItemCount(): Int {
+            return itemProvider.countItems()
+        }
+
+        override fun onBindViewHolder(holder: Holder, position: Int) {
+            holder.bind(itemProvider.getItem(position))
+        }
+    }
+
+    private inner class Holder(view: View): RecyclerView.ViewHolder(view){
+        fun bind(item: Item){
+            itemView.item_text.text = item.toString()
+            itemView.setOnClickListener {
+                startActivity(Intent(activity, ImageViewerActivity::class.java))
+                presenter.setLastItem(item)
+            }
+        }
     }
 }
